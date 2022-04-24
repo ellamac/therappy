f409e1c6f6068db4a2f1d4a36b940e0c8bb75802
Ella Mäkelä <ellamakela@Ella-MacBook-Pro.local>
2022-04-24 11:19:42 +0100

    debug banner off

--- therappy/lib/main.dart
+++ therappy/lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       home: const AppFrame(title: 'TherAppy'),
       theme: ThemeData(
